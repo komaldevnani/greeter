708568c740ff1e9c111bfcd266401dad45179e5c
Komal Devnani <komaldevnani00@gmail.com>
2018-11-09 18:44:18 +0530

    Only greet you if -n is given as argument

--- greetme.cpp
+++ greetme.cpp
@@ -1,36 +1,44 @@
 #include<iostream>
 #include<fstream>
+#include<cstring>
+#define FILENAME "Name.txt"
+
 using namespace std;
 int main(int argc,char* argv[])
 {
-    char name[20];
-    if(argc == 2)
+    char name[50];
+    if(argc == 2 && strcmp(argv[1],"-n")== 0)
     {
         cout<<"Whats your name:";
-        cin>>name;
+        // TODO: if name is empty, then do something
+        // TODO: remove unnecessary spaces
+        cin.getline(name,50);
         cout<<"Hello "<<name;
         fstream file;
-        file.open("Name.txt",ios::out);
+        file.open(FILENAME, ios::out);
         file<<name;
         file.close();
     }
     else if(argc == 1)
     {
         fstream file;
-        file.open("Name.txt",ios::in);
+        file.open(FILENAME, ios::in);
         if(!file)      //file doesnt exists
         {
-            cout<<"Sorry, I dont know your name";
+            cout<<"Sorry, I dont know your name"<<endl;
+            cout<<"Give a argument '-n' to enter name";
         }
         else
         {   
             cout<<"Hello "; 
-            while(!file.eof())
-            {
-                file>>name;      //storing file data in name variable
-                cout<<name;
-            }
+            file.getline(name,50);
+            cout<<name;
         }
         file.close();
+    }
+    else
+    {
+        cout<<"Invalid arguments "<<endl;
+        cout<<"Enter '-n' as argument if not signed in before  \n\t eg.\t./a.out -n \n or zero argument to greet you\n\t eg. \t ./a.out ";
     }
 }
